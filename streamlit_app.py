2b4b4aff161c76df2d6edfa28b5ef0d97d557875
alkibby76 <79749165+alkibby76@users.noreply.github.com>
2023-01-13 13:49:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner') 
-streamlit.header('Breakfast Menu') 
-streamlit.text('Omega 3 & Blueberry Oatmeal') 
-streamlit.text('Kale, Spinach & Rocket Smoothie') 
-streamlit.text('Hard Boiled Free-Range Egg') 
+streamlit.header('🍈Breakfast Favorites🍏') 
+streamlit.text('🫐🫐Omega 3 & Blueberry Oatmeal') 
+streamlit.text('🥬🥬Kale, Spinach & Rocket Smoothie') 
+streamlit.text('🥚Hard Boiled Free-Range Egg') 
+streamlit.text('🥑Avocado Toast') 
+
+streamlit.header('🍉🍇🍍🍒🍑 Build Your Own Fruit Smoothie') 
